b6771d0a9bc3722eb514b143d112b2207189f13b
Lucasr169 <lusca0204@gmail.com>
2023-06-20 20:07:43 -0300

    backendcriado

---
+++ backend-cliente/src/main/java/br/una/cliente/persistencia/ClienteRepository.java
@@ -0,0 +1,17 @@
+package br.una.cliente.persistencia;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.una.cliente.entidades.Cliente;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
+	List<Cliente> findByNome(String nome);
+	
+	List<Cliente> findByCpf(String cpf);
+
+	Cliente findById(long id);
+
+}
